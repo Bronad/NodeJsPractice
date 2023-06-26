68b7e09769abd3219ecd312e7d6547e3acc15335
Bronad <90397301+Bronad@users.noreply.github.com>
2023-06-26 11:45:31 +0200

    Small Changes. finsished first Lesson

--- Step1/server.js
+++ Step1/server.js
@@ -1,16 +1,23 @@
 console.log('Hello World')
-console.log(global)
+//console.log(global)
 
 const os = require('os')
 const path = require('path')
 
-console.log(os.type)
-console.log(os.homedir)
-console.log(os.version)
-console.log(__dirname)
+// Here is the whole Path is Displayed
 console.log(__filename)
-console.log(path.dirname(__filename))
+// Here only the filename is Displayed
 console.log(path.basename(__filename))
-console.log(path.extname(__filename))
 
+
+// console.log(os.type)
+// console.log(os.homedir)
+// console.log(os.version)
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+
+// Path is a great Object that keeps different parts of the Extension -> an Object
 console.log(path.parse(__filename))
