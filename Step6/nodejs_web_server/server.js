e6fbab1dc72062ddba013b25da0025fb1a69a84b
Bronad <90397301+Bronad@users.noreply.github.com>
2023-06-28 18:03:13 +0200

    Step 5 wont work anymore, but cleaner

--- Step6/nodejs_web_server/server.js
+++ Step6/nodejs_web_server/server.js
@@ -3,4 +3,8 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', { root: __dirname});
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
