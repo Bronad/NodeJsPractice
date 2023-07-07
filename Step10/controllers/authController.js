cebddba65e63b683eaf7040d089a560b5091676b
Bronad <90397301+Bronad@users.noreply.github.com>
2023-07-07 16:59:45 +0200

    Added verify JWT

--- Step10/controllers/authController.js
+++ Step10/controllers/authController.js
@@ -4,6 +4,12 @@ const usersDB = {
 }
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+
 const handleLogin = async (req, res) => {
     const {user, pwd} = req.body;
     // Check if Data was given
@@ -14,8 +20,35 @@ const handleLogin = async (req, res) => {
 
     const match = await bcrypt.compare(pwd, foundUser.password);
     if(match){
-        // create of JWTs -> for Safety 
-        res.json({'success': `User ${user} is logged in! `})
+        // create of JWTs -> for Safety
+        const accessToken = jwt.sign(
+            // Here u should never Use Passwords as they would easyly be optainable
+            {"username": foundUser.username},
+            process.env.ACCESS_TOKEN_SECERT ,
+            { expiresIn: '30s'}
+        );
+        const refreshToken = jwt.sign(
+            // Here u should never Use Passwords as they would easyly be optainable
+            {"username": foundUser.username},
+            process.env.REFRESH_TOKEN_SECERT,
+            { expiresIn: '1d'}
+        );
+        const otherUser = usersDB.users.filter(person => person.username !== foundUser.username);
+        const currentUser = {...foundUser, refreshToken};
+        usersDB.setUsers([...otherUserm, currentUser]);
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(usersDB.users)
+        );
+
+            //Cookies & local Storage is not safe for the Tokens.
+            // Best case in Memory
+            //Still gonna Try in Cookies tho
+            // Making a cookie here. httponly is not available in JS
+            // 
+        res.cookie('jwt', refreshToken, {httpOnly: true, maxAge: 24*60*60*1000});
+        // This is to Grab by the FrontEnd Dev
+        res.json({ accessToken });
     }else {
         res.sendStatus(401);
     }
